205034b4b6a020ce1284e609a2a4ef9ff4b389be
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-15 12:26:07 -0300

    feat: create new component category

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,13 +1,13 @@
-import { MaterialIcons } from '@expo/vector-icons'
-import { Pressable, Text } from 'react-native'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-import { styles } from './styles'
+import { colors } from '@/styles/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Pressable, Text } from 'react-native';
+import { styles } from './styles';
 
 export function Category() {
   return (
-    <Pressable>
-      <MaterialIcons name="code" size={16} color={Colors.gray[400]} />
+    <Pressable style={styles.container}>
+      <MaterialIcons name="code" size={16} color={colors.gray[400]} />
       <Text style={styles.name}>Projetos</Text>
     </Pressable>
-  )
+  );
 }
